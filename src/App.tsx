0c5443b88152838af527871fad56c1c43447eeb1
Marc-Henry Nanguy <marchenrynanguy1@gmail.com>
2023-02-04 02:53:49 +0100

    useReducer implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,53 @@
-import React, { useMemo, useState } from "react";
+import React, { useEffect, useReducer } from "react";
 import "./App.css";
 import { AddTodoForm } from "./components/add-todo-form";
-import {
-  FilterItemsSelect,
-  FilterStateEnum,
-} from "./components/filter-items-select";
+import { FilterItemsSelect } from "./components/filter-items-select";
 import { TodoList } from "./components/todo-list";
 import { Todo } from "./models/Todo.model";
+import {
+  initialTodosState,
+  TodosActions,
+  todosReducer,
+  UpdateTodoStatusPayload,
+} from "./stores/todos.store";
 
 function App() {
-  const [todos, setTodos] = useState<Todo[]>([
-    Todo.create("Reveiller a 13h"),
-    Todo.create("Manger"),
-    Todo.create("Glacier"),
-  ]);
-
-  const [filterState, setfilterState] = useState<FilterStateEnum>(
-    FilterStateEnum.ALL,
-  );
+  const [state, dispatch] = useReducer(todosReducer, initialTodosState);
 
   const addTodo = (todo: string) => {
-    setTodos([Todo.create(todo), ...todos]);
+    dispatch({
+      type: TodosActions.ADD,
+      payload: todo,
+    });
   };
 
   const updateTodoStatus = (todo: Todo, value: boolean) => {
-    const index = todos.findIndex((t) => t.id === todo.id);
-    const newTodos = [...todos];
-    newTodos[index] = { ...todo, done: value };
-    setTodos(newTodos);
+    dispatch({
+      type: TodosActions.UPDATE_STATUS,
+      payload: {
+        todo,
+        value,
+      } as UpdateTodoStatusPayload,
+    });
   };
 
-  const filterTodos = () => {
-    switch (filterState) {
-      case FilterStateEnum.COMPLETED:
-        return todos.filter((todo) => todo.done);
+  // const filterTodos = () => {
+  //   switch (filterState) {
+  //     case FilterStateEnum.COMPLETED:
+  //       return todos.filter((todo) => todo.done);
 
-      case FilterStateEnum.TODO:
-        return todos.filter((todo) => !todo.done);
-      default:
-        return todos;
-    }
-  };
+  //     case FilterStateEnum.TODO:
+  //       return todos.filter((todo) => !todo.done);
+  //     default:
+  //       return todos;
+  //   }
+  // };
 
-  const visibleTodos = useMemo(filterTodos, [todos, filterState]);
-
+  useEffect(() => {
+    dispatch({
+      type: TodosActions.REFRESH_FILTER,
+    });
+  }, [state.all]);
   return (
     <div className="container mx-auto py-4 max-sm:px-3">
       <h1 className="text-center text-blue-700 font-bold text-xl mb-3">
@@ -51,11 +55,18 @@ function App() {
       </h1>
       <AddTodoForm handler={addTodo} />
       <div className="w-full flex justify-end mt-4">
-        <FilterItemsSelect onChange={(state) => setfilterState(state)} />
+        <FilterItemsSelect
+          onChange={(state) =>
+            dispatch({
+              type: TodosActions.FILTER,
+              payload: state,
+            })
+          }
+        />
       </div>
       <div className="mt-4">
         <TodoList
-          todos={visibleTodos}
+          todos={state.visibleTodos}
           updateTodoStatusHandler={updateTodoStatus}
         />
       </div>
--- src/stores/todos.store.tsx
+++ src/stores/todos.store.tsx
@@ -1,1 +1,92 @@
-export enum TodosAction {}
+import { FilterStateEnum } from "../components/filter-items-select";
+import { Todo } from "../models/Todo.model";
+
+export enum TodosActions {
+  ADD = "ADD",
+  UPDATE_STATUS = "UPDATE_STATUS",
+  FILTER = "FILTER",
+  REFRESH_FILTER = "REFRESH_FILTER",
+}
+
+interface TodoState {
+  all: Todo[];
+  visibleTodos: Todo[];
+  filterStatus: FilterStateEnum;
+}
+
+interface TodoAction {
+  type: TodosActions;
+  payload?: any;
+}
+
+export interface UpdateTodoStatusPayload {
+  todo: Todo;
+  value: boolean;
+}
+
+const filterTodos = (
+  state: TodoState,
+  filterState: FilterStateEnum,
+): TodoState => {
+  switch (filterState) {
+    case FilterStateEnum.COMPLETED:
+      return {
+        ...state,
+        visibleTodos: state.all.filter((todo) => todo.done),
+        filterStatus: FilterStateEnum.COMPLETED,
+      };
+    case FilterStateEnum.TODO:
+      return {
+        ...state,
+        visibleTodos: state.all.filter((todo) => !todo.done),
+        filterStatus: FilterStateEnum.TODO,
+      };
+    default:
+      return {
+        ...state,
+        visibleTodos: state.all,
+        filterStatus: FilterStateEnum.ALL,
+      };
+  }
+};
+
+const updateTodoStatus = (
+  state: TodoState,
+  payload: UpdateTodoStatusPayload,
+) => {
+  const index = state.all.findIndex((t) => t.id === payload.todo.id);
+  const newTodos = [...state.all];
+  newTodos[index] = { ...payload.todo, done: payload.value };
+  return {
+    ...state,
+    all: [...newTodos],
+  };
+};
+
+export function todosReducer(state: TodoState, action: TodoAction) {
+  console.log(action);
+
+  switch (action.type) {
+    case TodosActions.ADD:
+      return {
+        ...state,
+        all: [Todo.create(action.payload as string), ...state.all],
+      };
+    case TodosActions.UPDATE_STATUS:
+      return updateTodoStatus(state, action.payload);
+    case TodosActions.FILTER:
+      return filterTodos(state, action.payload);
+    case TodosActions.REFRESH_FILTER:
+      console.log(state.filterStatus);
+
+      return filterTodos(state, state.filterStatus);
+    default:
+      throw new Error();
+  }
+}
+
+export const initialTodosState: TodoState = {
+  all: [],
+  filterStatus: FilterStateEnum.ALL,
+  visibleTodos: [],
+};
